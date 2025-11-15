52f1c30b2c7bbb50f9807a47633ad52cb1b27c61
David <blackholeton816@gmail.com>
2025-11-14 22:50:15 -0500

    Define the functions that is needed for the calculator to operate.

--- calculator.py
+++ calculator.py
@@ -4,8 +4,37 @@ calculator.py
 
 One function per operation, in order.
 """
+import math
+
+
 # First example
 def add(a, b): 
-    pass
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if b == 0:
+        raise ZeroDivisionError("Divisor can't be zero")
+    return a / b
+
+def log(a,b):
+    if b < 0 or a == 1 or a < 0:
+        raise ValueError("Log argument and base can't be zero or below and log base can't be equal to 1")
+    return math.log(a,b)
+
+def exp(a, b):
+    return a ** b
+
+
+
+
+
+
+
 
 
